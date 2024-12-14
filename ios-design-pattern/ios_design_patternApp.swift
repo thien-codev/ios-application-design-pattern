199e0819c622679dbc946154f4fa51d1adf81b12
Nguyen Thien <thiennguyen15dt1@gmail.com>
2024-12-14 21:19:18 +0700

    create source code

---
+++ ios-design-pattern/ios_design_patternApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_design_patternApp.swift
+//  ios-design-pattern
+//
+//  Created by Nguyen Thien on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_design_patternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
