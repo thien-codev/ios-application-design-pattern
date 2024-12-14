b5ccc6007d9c1e173954308565b916fc0bb0e621
Nguyen Thien <thiennguyen15dt1@gmail.com>
2024-12-15 02:58:33 +0700

    Singleton

--- ios-design-pattern/Creational/Singleton/Singleton.swift
+++ ios-design-pattern/Creational/Singleton/Singleton.swift
@@ -5,3 +5,11 @@
 //  Created by Nguyen Thien on 14/12/24.
 //
 
+/*
+ REASON:
+    - for component that make sense when have one object in the system
+        * Database repo
+        * Object factory
+    - when initializer is call expensively
+    - dont want anyone create additional object
+ */
